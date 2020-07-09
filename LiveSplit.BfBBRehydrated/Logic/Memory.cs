75f10e3582b3cf0845618a487923a5b897e27880
Steven Casper <sebastiancasper3@gmail.com>
2020-07-08 21:25:14 -0400

    Game version detection

--- LiveSplit.BfBBRehydrated/Logic/Memory.cs
+++ LiveSplit.BfBBRehydrated/Logic/Memory.cs
@@ -12,15 +12,23 @@ namespace LiveSplit.BfBBRehydrated.Logic
             public int SpatulaCount;
             public Level Level;
         }
+
+        public enum Version
+        {
+            Revision603296 = 603296,
+            Revision603442 = 603442
+        }
+        
+        public static Version GameVersion { get; private set; }
         
         public static bool IsLoading => IsHooked && _isLoadingDP.Deref<bool>(Game);
-        private static readonly DeepPointer _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0331A650, 0x20, 0x1D0);
+        private static DeepPointer _isLoadingDP;
         
         public static int SpatulaCount => IsHooked ? _spatulaCountDP.Deref<int>(Game) : 0;
-        private static readonly DeepPointer _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415DB8, 0x8, 0x6E0);
+        private static DeepPointer _spatulaCountDP;
         
-        public static Level CurrentLevel => IsHooked ? MapsHelper.Paths[_currentMapDP.DerefString(Game, 150)] : Level.Any;
-        private static readonly DeepPointer _currentMapDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03416E10, 0x8A8, 0x0);
+        public static Level CurrentLevel => IsHooked ? MapsHelper.Paths[_currentLevelDP.DerefString(Game, 150)] : Level.Any;
+        private static DeepPointer _currentLevelDP;
         
         public static Process Game { get; private set; }
         public static bool IsHooked { get; private set; }
@@ -48,6 +56,27 @@ namespace LiveSplit.BfBBRehydrated.Logic
                     return;
                 
                 Game = processes.First();
+                
+                //Determine Game Version
+                int moduleMemorySize = Game.MainModule.ModuleMemorySize;
+                switch (moduleMemorySize)
+                {
+                    //Revision 603296
+                    case 58867712:
+                        _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0338B8D0, 0x20, 0x1A0);
+                        _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03487038, 0x8, 0x6E0);
+                        _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x3488090, 0x8A8, 0x0);
+                        GameVersion = Version.Revision603296;
+                        break;
+                    //Revision 603442
+                    case 58372096:
+                        _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0331A650, 0x20, 0x1D0);
+                        _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415DB8, 0x8, 0x6E0);
+                        _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03416E10, 0x8A8, 0x0);
+                        GameVersion = Version.Revision603442;
+                        break;
+                }
+                
                 IsHooked = true;
             }
         }
