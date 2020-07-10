834a9cfe134e2fd8ad332a8b421931a06e24e294
Steven Casper <sebastiancasper3@gmail.com>
2020-07-10 02:57:30 -0400

    Move strings to resource file

--- LiveSplit.BfBBRehydrated/UI/RehydratedAutosplitter.cs
+++ LiveSplit.BfBBRehydrated/UI/RehydratedAutosplitter.cs
@@ -27,16 +27,11 @@ namespace LiveSplit.BfBBRehydrated.UI
             
             // Make sure the user is using the correct Timing Method
             if (_state.CurrentTimingMethod == TimingMethod.RealTime)
-            {        
-                var timingMessage = MessageBox.Show
-                (
-                    "This game uses Time without Loads (Game Time) as the main timing method.\n"+
-                    "LiveSplit is currently set to show Real Time (RTA).\n"+
-                    "Would you like to set the timing method to Game Time?",
-                    "LiveSplit | BFBB Rehydrated",
-                    MessageBoxButtons.YesNo,MessageBoxIcon.Question
-                );
-                if (timingMessage == DialogResult.Yes)
+            {
+                var messageResponse = MessageBox.Show(strings.GameTime_Warning, strings.Component_Caption,
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                
+                if (messageResponse == DialogResult.Yes)
                 {
                     _state.CurrentTimingMethod = TimingMethod.GameTime;
                 }
@@ -71,19 +66,15 @@ namespace LiveSplit.BfBBRehydrated.UI
             if (settings["ScriptPath"] != null)
             {
                 //We have upgraded from the ASL script to this component.
-                if (MessageBox.Show
-                (
-                "The autosplitter has been updated, would you like to have your settings converted?", "LiveSplit | BFBB Rehydrated",
-                    MessageBoxButtons.YesNo, MessageBoxIcon.Question
-                ) == DialogResult.Yes)
+                var messageResponse = MessageBox.Show(strings.Settings_Conversion, strings.Component_Caption,
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (messageResponse == DialogResult.Yes)
                 {
                     _settings.PortSettings(settings);
                     MessageBox.Show
                     (
-                        "The conversion process has completed. You may want to check your settings to make sure everything is still correct.\n\n" +
-                        "If you previously used the delay split option, you will need to manually setup Level Transition splits to achieve the same behavior.\n\n" +
-                        "If you previously used a mixture of manual and automatic splits you will need to manually reorder your split settings.",
-                        "LiveSplit | BFBB Rehydrated", MessageBoxButtons.OK, MessageBoxIcon.Information
+                        strings.Settings_Conversion_Completed,
+                        strings.Component_Caption, MessageBoxButtons.OK, MessageBoxIcon.Information
                     );
                 }
             }
