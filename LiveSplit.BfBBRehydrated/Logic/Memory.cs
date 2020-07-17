e8f90405f68681c903b6865314def55d55ebfeec
Steven Casper <sebastiancasper3@gmail.com>
2020-07-17 07:09:46 -0400

    Implement fallback if unsupported version is hooked to prevent accessing null/invalid pointers

--- LiveSplit.BfBBRehydrated/Logic/Memory.cs
+++ LiveSplit.BfBBRehydrated/Logic/Memory.cs
@@ -15,6 +15,7 @@ namespace LiveSplit.BfBBRehydrated.Logic
 
         public enum Version
         {
+            Unsupported = 0,
             Revision603296 = 603296,
             Revision603442 = 603442,
             Revision603899 = 603899
@@ -48,45 +49,57 @@ namespace LiveSplit.BfBBRehydrated.Logic
         /// </summary>
         public static void HookProcess()
         {
-            IsHooked = Game != null && !Game.HasExited;
-            if (!IsHooked)
+            if (Game != null)
             {
-                Process[] processes = Process.GetProcessesByName("Pineapple-Win64-Shipping");
+                if(!Game.HasExited) return;
+
+                // The game has exited and we need to clean up
+                Game.Dispose();
+                Game = null;
+            
+                IsHooked = false;
+                return;
+            }
+
+            Process[] processes = Process.GetProcessesByName("Pineapple-Win64-Shipping");
                 
-                if (processes.Length <= 0)
-                    return;
+            if (processes.Length <= 0)
+                return;
                 
-                Game = processes.First();
+            Game = processes.First();
                 
-                //Determine Game Version
-                int moduleMemorySize = Game.MainModule.ModuleMemorySize;
-                switch (moduleMemorySize)
-                {
-                    //Revision 603296
-                    case 58867712:
-                        _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0338B8D0, 0x20, 0x1A0);
-                        _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03487038, 0x8, 0x6E0);
-                        _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x3488090, 0x8A8, 0x0);
-                        GameVersion = Version.Revision603296;
-                        break;
-                    //Revision 603442
-                    case 58372096:
-                        _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0331A650, 0x20, 0x1D0);
-                        _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415DB8, 0x8, 0x6E0);
-                        _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03416E10, 0x8A8, 0x0);
-                        GameVersion = Version.Revision603442;
-                        break;
-                    // Revision 603899
-                    case 58363904:
-                        _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03319550, 0x20, 0x170);
-                        _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03166BE8, 0x8, 0x7A0);
-                        _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415D10, 0x8A8, 0x0);
-                        GameVersion = Version.Revision603899;
-                        break;
-                        
-                }
-                
-                IsHooked = true;
+            // Determine Game Version
+            int moduleMemorySize = Game.MainModule.ModuleMemorySize;
+            switch (moduleMemorySize)
+            {
+                // Revision 603296
+                case 58867712:
+                    _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0338B8D0, 0x20, 0x1A0);
+                    _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03487038, 0x8, 0x6E0);
+                    _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x3488090, 0x8A8, 0x0);
+                    GameVersion = Version.Revision603296;
+                    IsHooked = true;
+                    break;
+                // Revision 603442
+                case 58372096:
+                    _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0331A650, 0x20, 0x1D0);
+                    _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415DB8, 0x8, 0x6E0);
+                    _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03416E10, 0x8A8, 0x0);
+                    GameVersion = Version.Revision603442;
+                    IsHooked = true;
+                    break;
+                // Revision 603899
+                case 58363904:
+                    _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03319550, 0x20, 0x170);
+                    _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03166BE8, 0x8, 0x7A0);
+                    _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415D10, 0x8A8, 0x0);
+                    GameVersion = Version.Revision603899;
+                    IsHooked = true;
+                    break;
+                default:
+                    GameVersion = Version.Unsupported;
+                    IsHooked = false;
+                    break;
             }
         }
     }
