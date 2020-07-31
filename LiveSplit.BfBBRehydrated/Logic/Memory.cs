add87fd543189074a84773597643825be24427fe
Steven Casper <sebastiancasper3@gmail.com>
2020-07-31 06:12:23 -0400

    Implement Sock Count pointers

--- LiveSplit.BfBBRehydrated/Logic/Memory.cs
+++ LiveSplit.BfBBRehydrated/Logic/Memory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using LiveSplit.ComponentUtil;
 
 namespace LiveSplit.BfBBRehydrated.Logic
@@ -10,6 +11,7 @@ namespace LiveSplit.BfBBRehydrated.Logic
         public struct MemoryState
         {
             public bool IsLoading;
+            public int SockCount;
             public int SpatulaCount;
             public Level Level;
         }
@@ -25,11 +27,8 @@ namespace LiveSplit.BfBBRehydrated.Logic
         public static Version GameVersion { get; private set; }
         
         public static bool IsLoading => IsHooked && _isLoadingDP.Deref<bool>(Game);
-        private static DeepPointer _isLoadingDP;
-        
+        public static int SockCount => IsHooked ? _sockCountDp.Deref<int>(Game) : 0;
         public static int SpatulaCount => IsHooked ? _spatulaCountDP.Deref<int>(Game) : 0;
-        private static DeepPointer _spatulaCountDP;
-
         public static Level CurrentLevel
         {
             get
@@ -49,6 +48,10 @@ namespace LiveSplit.BfBBRehydrated.Logic
                 return Level.Any;
             }
         }
+
+        private static DeepPointer _isLoadingDP;
+        private static DeepPointer _sockCountDp;
+        private static DeepPointer _spatulaCountDP;
         private static DeepPointer _currentLevelDP;
         
         public static Process Game { get; private set; }
@@ -63,7 +66,7 @@ namespace LiveSplit.BfBBRehydrated.Logic
         /// <returns></returns>
         public static MemoryState GetState()
         {
-            return new MemoryState {IsLoading = IsLoading, SpatulaCount = SpatulaCount, Level = CurrentLevel};
+            return new MemoryState {IsLoading = IsLoading, SockCount = SockCount, SpatulaCount = SpatulaCount, Level = CurrentLevel};
         }
         
         /// <summary>
@@ -104,6 +107,7 @@ namespace LiveSplit.BfBBRehydrated.Logic
                 // Revision 603296
                 case 58867712:
                     _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0338B8D0, 0x20, 0x1A0);
+                    _sockCountDp = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03487038, 0x8, 0x6DC);
                     _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03487038, 0x8, 0x6E0);
                     _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x3488090, 0x8A8, 0x0);
                     GameVersion = Version.Revision603296;
@@ -112,6 +116,7 @@ namespace LiveSplit.BfBBRehydrated.Logic
                 // Revision 603442
                 case 58372096:
                     _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x0331A650, 0x20, 0x1D0);
+                    _sockCountDp = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415DB8, 0x8, 0x6DC);
                     _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415DB8, 0x8, 0x6E0);
                     _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03416E10, 0x8A8, 0x0);
                     GameVersion = Version.Revision603442;
@@ -120,6 +125,7 @@ namespace LiveSplit.BfBBRehydrated.Logic
                 // Revision 603899
                 case 58363904:
                     _isLoadingDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03319550, 0x20, 0x170);
+                    _sockCountDp = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03166BE8, 0x8, 0x79C);
                     _spatulaCountDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03166BE8, 0x8, 0x7A0);
                     _currentLevelDP = new DeepPointer("Pineapple-Win64-Shipping.exe", 0x03415D10, 0x8A8, 0x0);
                     GameVersion = Version.Revision603899;
